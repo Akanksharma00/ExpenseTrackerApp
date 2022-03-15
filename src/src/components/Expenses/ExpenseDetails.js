3c126cdfdf45468f0e9792c26af1072be492e3fa
Akanksharma00 <56409268+Akanksharma00@users.noreply.github.com>
2022-03-15 19:13:05 +0530

    Components in React

---
+++ src/src/components/Expenses/ExpenseDetails.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+
+const ExpenseDetails = (props) => {
+    return(
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <p>{props.location}</p>
+            <div className="expense-item__price">
+                {props.amount}
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseDetails;
