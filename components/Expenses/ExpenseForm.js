bd0b3b1f8b3fdf9a64cc18954952d03f78040bcf
Akanksharma00 <56409268+Akanksharma00@users.noreply.github.com>
2022-03-27 23:52:30 +0530

    Rendering add new form conditionally

--- components/Expenses/ExpenseForm.js
+++ components/Expenses/ExpenseForm.js
@@ -87,6 +87,7 @@ const ExpenseForm = (props) => {
             </div>
             <div className='new-expense__action'>
                 <button type='submit' className='new-expense__actions'>Add Expense</button>
+                <button type='button' className='new-expense__actions' onClick={props.onCancel}>Cancel</button> 
             </div>
         </form>
     );
